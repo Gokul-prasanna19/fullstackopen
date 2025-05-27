75e68392b5beca4ab21449cb80ed56bb837c1da5
Gokul-prasanna19 <kgokulaprasanna@gmail.com>
2025-05-27 22:15:31 +0530

    Add part1 folder with intro demo code

---
+++ part1/introdemo/src/Total.jsx
@@ -0,0 +1,9 @@
+const Total = (props) => {
+    return (
+        <div>
+            Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+        </div>
+    )
+}
+
+export default Total
